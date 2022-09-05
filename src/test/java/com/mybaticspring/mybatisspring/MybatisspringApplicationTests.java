33de0d42b01a300e5eca2d6ede94f001bf33299e
nunamed <1736633080@qq.com>
2022-09-05 22:09:36 +0800

    init

---
+++ src/test/java/com/mybaticspring/mybatisspring/MybatisspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybaticspring.mybatisspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
